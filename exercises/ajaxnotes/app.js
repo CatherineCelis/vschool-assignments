b87cc16f6dd4dae3f35d574d752e7ea6ef60ad7b
Catherine Celis <catherine.celis7@gmail.com>
2018-03-15 20:23:18 -0600

    design a blog

---
+++ exercises/ajaxnotes/app.js
@@ -0,0 +1,25 @@
+var todoList = document.getElementById("todo-list");
+
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {  
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var jsonData = xhr.responseText;
+        var data = JSON.parse(jsonData);
+        console.log(data);
+        for (var i = 0; i<data.length; i++) {
+            var todoObj = data[i];
+            var li = document.createElement("li");
+            li.innerHTML = todoObj.title;
+            todoList.appendChild(li);
+        }
+    }
+}
+
+xhr.open("GET", "https://api.vschool.io/catherine/todo/", true);
+xhr.send();
+
+// when the page loads send a request for some data
+// with that data convert it to javascript
+// create the necessary html elements whioch will:
+// display that data
