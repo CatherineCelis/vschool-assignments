e10fa4999e92aab1db6822ebd56b10df0fd40ddb
Catherine Celis <catherine.celis7@gmail.com>
2018-03-23 16:33:16 -0600

    forception submit

--- exercises/forception/index.js
+++ exercises/forception/index.js
@@ -1,22 +1,20 @@
-var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];  
-var alphabet = "abcdefghijklmnopqrstuvwxyz";  
+  
 // create a function that will create a new array
 // inside this array,each index of the people array will be followed by a colin
 // then followed by the entire alphabet string after each colin.
 // loop within a loop, make sure to return our new array
 // console.log our function and pass in the correct parameters
-
-
-
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];  
+var alphabet = "abcdefghijklmnopqrstuvwxyz";  
 
 function forception(arr, str) {
     var newArr = [];
-    for(var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         newArr.push(arr[i] + ":");
-        for (var j = 0; j < str.length; i++) {
+        for (var j = 0; j < str.length; j++) {
             newArr.push(str[j]);
         }
     }
     return newArr;
-}
-console.log(forception(people,alphabet));
+} 
+console.log(forception(people, alphabet));
